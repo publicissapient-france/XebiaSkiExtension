590d028aabd08050b8400600e32eebdfc9bdecdc
pjechris <jcpastant@xebia.fr>
2014-12-01 00:22:27 +0100

    show image as background

--- XebiaSki WatchKit Extension/InterfaceController.swift
+++ XebiaSki WatchKit Extension/InterfaceController.swift
@@ -8,22 +8,37 @@
 
 import WatchKit
 import Foundation
-
+import XebiaSkiFramework
 
 class InterfaceController: WKInterfaceController {
+    @IBOutlet weak var labelTemperature: WKInterfaceLabel!
+    @IBOutlet weak var labelDay: WKInterfaceLabel!
+    @IBOutlet weak var labelResortName: WKInterfaceLabel!
+    @IBOutlet weak var group: WKInterfaceGroup!
+//    private let photoDownloadManager = PhotoDownloadManager(photoURL: NSURL(string: "http://www.trinum.com/ibox/ftpcam/mega_les-arcs_arcabulle.jpg")!)
+
 
     override init(context: AnyObject?) {
         // Initialize variables here.
         super.init(context: context)
         
-        // Configure interface objects here.
-        NSLog("%@ init", self)
+        self.labelTemperature.setText(42.description + "°C")
     }
 
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
-        NSLog("%@ will activate", self)
+
+        let img = UIImage(named: "les_arcs")
+        let filter = CIFilter(name:"CIGaussianBlur")
+
+        filter.setValue(CIImage(image: img), forKey: kCIInputImageKey)
+
+        let outputImg = filter.outputImage
+        var context = CIContext(options:nil)
+        let cgimg = context.createCGImage(outputImg, fromRect: CGRectMake(0, 0, 800, 600))
+
+        self.group.setBackgroundImage(UIImage(CGImage: cgimg))
     }
 
     override func didDeactivate() {
